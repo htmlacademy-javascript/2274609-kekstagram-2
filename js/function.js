ece4407ef51ba59d14f8a20fc7deb07de06542ba
Ilia Konev <122528494+bearBenjamin@users.noreply.github.com>
2025-10-29 13:10:38 +0300

    feat: добавил логику решения дополнительных функций в function.js

--- js/function.js
+++ js/function.js
@@ -59,3 +59,78 @@ isPalindromeMetod();
 isPalindromeCycle();
 getPositiveIntegeNumber();
 getNumber();
+
+/* Решение дополнительных задач */
+
+/* 2626.Array Reduce Transform */
+function reduce (nums, fn, init) {
+  if (nums.length === 0) {
+    return init;
+  }
+
+  let result = init;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    result = fn(result, nums[i]);
+  }
+
+  return result;
+}
+
+/* 2634.Filter Elements from Array */
+function filter (arr, fn) {
+  const result = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    const value = fn(arr[i], i);
+
+    if (value) {
+      result.push(arr[i]);
+    }
+
+  }
+
+  return result;
+}
+
+/* 2620.Counter */
+function createCounter(n) {
+  let currentN = n;
+
+  return function() {
+    const saveCurrentN = currentN;
+    currentN += 1;
+    return saveCurrentN;
+  };
+}
+
+/* 2635.Apply Transform Over Each Element in Array */
+function map (arr, fn) {
+  const resultArr = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    const item = fn(arr[i], i);
+    resultArr.push(item);
+  }
+
+  return resultArr;
+}
+
+/* 2648.Generate Fibonacci Sequence */
+const fibGenerator = function*() {
+  let previous = 0;
+  let current = 1;
+  while (true) {
+    yield previous;
+    const temp = current;
+    current = current + previous;
+    previous = temp;
+  }
+
+};
+
+reduce();
+filter();
+createCounter();
+map();
+fibGenerator();
