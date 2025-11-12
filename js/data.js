806a11bcf4a42dd577a12f16edd7a24df3546433
Ilia Konev <122528494+bearBenjamin@users.noreply.github.com>
2025-11-12 17:24:43 +0300

    refactor: привел в порядок разбиение на модули

--- js/data.js
+++ js/data.js
@@ -1,12 +1,5 @@
-import { getNumbers, getDataPhotos } from './function.js';
-
-const LENGTH_PHOTO_DATAS = 25;
-const AVATAR_START_NUM = 1;
-const AVATAR_END_NUM = 6;
-const LIKE_START_NUM = 15;
-const LIKE_END_NUM = 200;
-const COMMENT_START_NUM = 1;
-const COMMENT_END_NUM = 3;
+import { getRandomNumber, getNumbers } from './utils.js';
+import { LENGTH_PHOTO_DATAS, LENGHT_COMMENTS, START_NUM, AVATAR_END_NUM, LIKE_START_NUM, LIKE_END_NUM, COMMENT_END_NUM } from './const.js';
 
 const DESCRIPTIONS = [
   'Моя бабушка курит трубку',
@@ -65,23 +58,78 @@ const NAMES = [
   'Юрец'
 ];
 
-const idDatas = getNumbers(1, 25);
-const idComments = getNumbers(1, 150);
+const ID_DATAS = getNumbers(START_NUM, LENGTH_PHOTO_DATAS);
+const ID_COMMENTS = getNumbers(START_NUM, LENGHT_COMMENTS);
 
 const parameter = {
   lengthArr: LENGTH_PHOTO_DATAS,
-  avatarStartNum: AVATAR_START_NUM,
+  avatarStartNum: START_NUM,
   avatarEndNum: AVATAR_END_NUM,
   likeStartNum: LIKE_START_NUM,
   likeEndNum: LIKE_END_NUM,
-  commentStartNum: COMMENT_START_NUM,
+  commentStartNum: START_NUM,
   commentEndNum: COMMENT_END_NUM,
-  idDatas: idDatas,
-  idComments: idComments,
+  idDatas: ID_DATAS,
+  idComments: ID_COMMENTS,
   messages: MESSAGES,
   names: NAMES,
   descriptions: DESCRIPTIONS,
 };
+
+function getComments (commentsSum, idComments, avatarStartNum, avatarEndNum, message, name) {
+  const comments = Array.from({ length: commentsSum }, () => {
+    const uniqueCommentId = idComments.pop();
+    return {
+      id: uniqueCommentId,
+      avatar: `img/avatar-${getRandomNumber(avatarStartNum, avatarEndNum)}.svg`,
+      message: message[getRandomNumber(0, message.length - 1)],
+      name: name[getRandomNumber(0, name.length - 1)],
+    };
+  });
+  return comments;
+}
+
+function getDataPhotos (parametres) {
+  const {
+    lengthArr,
+    avatarStartNum,
+    avatarEndNum,
+    likeStartNum,
+    likeEndNum,
+    commentStartNum,
+    commentEndNum,
+    idDatas,
+    idComments,
+    messages,
+    names,
+    descriptions
+  } = parametres;
+
+  const photoDatas = Array.from({ length: lengthArr }, (_, index) => {
+    const currentId = idDatas[index];
+
+    const commentsSum = getRandomNumber(commentStartNum, commentEndNum);
+    const comments = getComments(
+      commentsSum,
+      idComments,
+      avatarStartNum,
+      avatarEndNum,
+      messages,
+      names
+    );
+
+    return {
+      id: currentId,
+      url: `photos/${currentId}.jpg`,
+      description: descriptions[getRandomNumber(0, descriptions.length - 1)],
+      like: getRandomNumber(likeStartNum, likeEndNum),
+      comments: comments,
+    };
+  });
+
+  return photoDatas;
+}
+
 
 export const dataPhotos = getDataPhotos(parameter);
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import './utils.js';
 import './function.js';
 import './data.js';
 
