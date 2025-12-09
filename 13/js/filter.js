51fb0982a133c5ccca372691b272e5f253eca484
Keks <keks@htmlacademy.ru>
2025-12-09 18:52:59 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/filter.js
@@ -0,0 +1,83 @@
+import { renderPreview } from './preview-photo.js';
+import { getCurrentData } from './full-photo.js';
+import { debounce } from './utils.js';
+
+const pictureContainer = document.querySelector('.pictures.container');
+
+const filterContainer = document.querySelector('.img-filters');
+const filterBtns = filterContainer.querySelectorAll('.img-filters__button');
+
+const cleanBtn = (btns) => {
+  btns.forEach((btn) => {
+    btn.classList.remove('img-filters__button--active');
+  });
+};
+
+const cleanPictureContainer = () => {
+  const pictures = pictureContainer.querySelectorAll('.picture');
+
+  pictures.forEach((picture) => {
+    picture.remove();
+  });
+};
+
+const showPhotos = (dataPhotos) => {
+  renderPreview(dataPhotos);
+  getCurrentData(dataPhotos);
+};
+
+const showPhotosDebounce = debounce((data) => showPhotos(data));
+
+const shuffleData = (dataPhotos, count = 10) => {
+  const array = [...dataPhotos];
+  let randomIndex;
+
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    randomIndex = Math.floor(Math.random() * (i + 1));
+
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+
+  return array.slice(0, Math.min(count, array.length));
+};
+
+const sortMessageAmount = (dataPhotos) => {
+  const array = [...dataPhotos];
+  const messageAmountSort = (messageA, messageB) => messageB.comments.length - messageA.comments.length;
+  const sortArray = array.sort(messageAmountSort);
+  return sortArray;
+};
+
+const getFilterPhotos = (dataPhotos) => {
+  showPhotos(dataPhotos);
+  filterContainer.classList.remove('img-filters--inactive');
+
+  filterBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      cleanBtn(filterBtns);
+      cleanPictureContainer();
+      btn.classList.add('img-filters__button--active');
+
+      switch(btn.id) {
+        case ('filter-default'):
+          showPhotosDebounce(dataPhotos);
+          break;
+
+        case ('filter-random'): {
+          const newDataPhotos = shuffleData(dataPhotos);
+          showPhotosDebounce(newDataPhotos);
+          break;
+        }
+
+        case ('filter-discussed'): {
+          const newDataPhotos = sortMessageAmount(dataPhotos);
+          showPhotosDebounce(newDataPhotos);
+          break;
+        }
+      }
+    });
+  });
+};
+
+
+export { getFilterPhotos };
---
+++ 13/js/init.js
@@ -0,0 +1,17 @@
+import { initFormSubmit } from './form.js';
+import { getData } from './fetch.js';
+import { showAlert } from './modal-message-user.js';
+import { getFilterPhotos } from './filter.js';
+
+const init = () => {
+  getData()
+    .then((dataPhotos) => {
+      getFilterPhotos(dataPhotos);
+    })
+    .catch(() => {
+      showAlert();
+    });
+  initFormSubmit();
+};
+
+export { init };
